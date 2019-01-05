0e162d877f7728656d204766ea04ba1d24aedfe1
Pantita Palittapongarnpim <panpalitta@gmail.com>
2019-01-04 18:59:56 -0700

    Revert "1.0.1 Documents"
    This reverts commit 860dd4961030c3105b28e383658fdd338f72ee7c.

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['avg_5ffinal_5fselect',['avg_Final_select',['../classOptAlg.html#a00eb4ffc7e7ce8fe072ef90cd9e8dc52',1,'OptAlg']]],
+  ['avg_5ffinal_5fselect',['avg_Final_select',['../classOptAlg.html#a93f5a6d878a952ace1e59c9535f169f3',1,'OptAlg']]],
   ['avg_5ffitness',['avg_fitness',['../classProblem.html#a5a889257574b59d70c30e77f96657633',1,'Problem']]]
 ];
